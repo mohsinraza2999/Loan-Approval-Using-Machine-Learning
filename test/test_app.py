002f99eda9496425b285dd1ca4ada2e90174cbab
mohsinraza2999 <mohsinraza2999@gmail.com>
2025-09-29 05:37:43 -0700

    path issue solved

--- test/test_app.py
+++ test/test_app.py
@@ -1,4 +1,6 @@
 from fastapi.testclient import TestClient
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from main import app
 import numpy as np
 client = TestClient(app)

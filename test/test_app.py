658c3c009ded3e2d594855fefd13fc9300112239
mohsinraza2999 <mohsinraza2999@gmail.com>
2025-09-29 23:06:48 -0700

    add another test

--- test/test_app.py
+++ test/test_app.py
@@ -2,14 +2,32 @@ from fastapi.testclient import TestClient
 import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from main import app
-import numpy as np
 client = TestClient(app)
 
-sample = [0,0.0, 1, 0, 5849, 0.0, 1200, 360.0, 1.0, 1]
-prepared_sample = np.array(sample).reshape(1, -1)
 results=['Loan is Approved','Loan is Not Approved']
-def test_predict():
+def test_app():
     
-    response = client.post("/")
+    response = client.get("/")
+    print("RESPONSE : \n",response)
     assert response.status_code == 200
-    #assert response.json()["prediction"] in results
+
+def test_prediction():
+    payload = {
+        "feature1": 0.0,
+        "feature2": 1,
+        "feature3": 0,
+        "feature4": 5849,
+        "feature5": 0.0,
+        "feature6": 1200,
+        "feature7": 360.0,
+        "feature8": 1.0,
+        "feature9": 1
+    }
+    for key in payload.keys():
+        payload[key]=str(payload[key])
+    response = client.post("/submit", data=payload)
+    print("RESPONSE :\n",response)
+    assert response.status_code == 200
+    assert response.json()["prediction"] in results
+
+
